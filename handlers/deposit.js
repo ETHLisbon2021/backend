95c023655ff81d7a2dd796f8727b9275aac0ffd3
poegva <poegva@mail.ru>
2021-10-24 12:57:14 +0300

    Update deposit.js

--- handlers/deposit.js
+++ handlers/deposit.js
@@ -3,6 +3,12 @@ const Sale = require("../models/Sale");
 async function handleDeposit(tokenAddress, user, amount) {
     const sale = await Sale.findOne({ saleId: tokenAddress.toLowerCase() });
 
+    for (let i = 0; i < sale.users.length; i++) {
+        if (sale.users[i].address.toLowerCase() === user.toLowerCase()) {
+            return;
+        }
+    }
+
     sale.users.push({
         address: user.toLowerCase(),
         amount: amount
